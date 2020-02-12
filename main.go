c6f8264588b1548982a9e1ea500cd2004bc47caa
jenchieh <jcs090218@gmail.com>
2020-02-12 23:31:47 +0800

    Add core.

---
+++ main.go
@@ -0,0 +1,18 @@
+/**
+ * $File: main.go $
+ * $Date: 2020-02-12 23:15:32 $
+ * $Revision: $
+ * $Creator: Jen-Chieh Shen $
+ * $Notice: See LICENSE.txt for modification and distribution information
+ *                   Copyright © 2020 by Shen, Jen-Chieh $
+ */
+
+package main
+
+import (
+	"lib"
+)
+
+func main() {
+	lib.Say()
+}
