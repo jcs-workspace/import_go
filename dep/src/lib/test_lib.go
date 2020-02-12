f076771806611521ad130a76dbbb8d306ea33485
jenchieh <jcs090218@gmail.com>
2020-02-12 23:31:35 +0800

    Add lib

---
+++ dep/src/lib/test_lib.go
@@ -0,0 +1,16 @@
+/**
+ * $File: test_lib.go $
+ * $Date: 2020-02-12 23:18:45 $
+ * $Revision: $
+ * $Creator: Jen-Chieh Shen $
+ * $Notice: See LICENSE.txt for modification and distribution information
+ *                   Copyright © 2020 by Shen, Jen-Chieh $
+ */
+
+package lib
+
+import "fmt"
+
+func Say() {
+	fmt.Println("Hello World!~")
+}
